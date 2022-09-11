efda1d004a50ab005b3bfd3b0c8f8eda3fdf50a9
ThanhHungA33367 <a33367@thanglong.edu.vn>
2022-09-11 20:12:38 +0700

    update

--- admin/san_pham/process_update.php
+++ admin/san_pham/process_update.php
@@ -1,33 +1,30 @@
-<?php require '../check_admin.php' ?>
-<?php 
+<?php require '../check_admin.php'?>
+<?php
 $id = $_POST['id'];
 $nha_san_xuat_id = $_POST['nha_san_xuat_id'];
 $ten = $_POST['ten'];
 $mo_ta = $_POST['mo_ta'];
 $gia = $_POST['gia'];
 $anh_moi = $_FILES['anh_moi'];
-if($anh_moi['size'] >0){
+if ($anh_moi['size'] > 0) {
 	$folder = 'anh/';
 	$file_extension = explode('.', $anh_moi["name"])[1];
-	$file_name = time(). '.' .$file_extension;
-	$target_file = $folder . $file_name ;
+	$file_name = time() . '.' . $file_extension;
+	$target_file = $folder . $file_name;
 	move_uploaded_file($anh_moi["tmp_name"], $target_file);
-}
-else{
+} else {
 	$file_name = $_POST['anh_cu'];
 }
 
-
 require '../connect.php';
-$sql ="update san_pham
-set 
+$sql = "update san_pham
+set
 ten = '$ten',
 gia = '$gia',
 mo_ta = '$mo_ta',
 nha_san_xuat_id = '$nha_san_xuat_id',
 anh = '$file_name'
-where 
+where
 id = '$id'";
-die($sql);
-mysqli_query($ket_noi,$sql);
+mysqli_query($ket_noi, $sql);
 mysqli_close($ket_noi);
--- admin/san_pham/xoa.php
+++ admin/san_pham/xoa.php
@@ -1,8 +1,8 @@
-<?php require '../check_admin.php' ?>
-<?php  
+<?php require '../check_admin.php'?>
+<?php
 $id = $_GET['id'];
 require '../connect.php';
-$sql ="delete from san_pham where id = '$id'";
-header('location:index.php');
-mysqli_query($ket_noi,$sql);
+$sql = "delete from san_pham where id = '$id'";
+mysqli_query($ket_noi, $sql);
+
 mysqli_close($ket_noi);
